880f6c72415f3ff27f08e990ab212d56986d908f
Douglas Borges <douglasborges19@gmail.com>
2022-12-13 23:59:56 -0300

    add inheritance-02

---
+++ inheritance-02/Course/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Course.Entities;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            //UPCASTING
+
+            //BusinessAccount para Account
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+
+            //SavingsAccount para Account
+            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+            //DOWNCASTING
+
+            //Para especializar é necessário converter explicitamente
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
+            //InvalidCastException
+            //SavingsAccount não é compatível com BusinessAccount
+            //BusinessAccount acc5 = (BusinessAccount)acc3;
+
+            //Utilizar o "is" para verificar se objeto é instância de determinada classe
+
+            if (acc3 is BusinessAccount)
+            {
+                BusinessAccount acc5 = (BusinessAccount)acc3;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan!");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                //SavingsAccount acc5 = (SavingsAccount)acc3;
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
+        }
+    }
+}
