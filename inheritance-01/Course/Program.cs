108778cc5ed8f0b8f8f2064ee18a5b6b0adc3348
Douglas Borges <douglasborges19@gmail.com>
2022-12-13 23:09:27 -0300

    initial commit

---
+++ inheritance-01/Course/Program.cs
@@ -0,0 +1,18 @@
+﻿using Course.Entities;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(1234, "Bob Brown", 150.00, 500.00);
+
+            account.Deposit(150.00);
+            account.WithDraw(100.00);
+            account.Loan(500.00);
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
