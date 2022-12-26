b9d5d0627eca2d5f07f2d41d86c32a30e393e654
Douglas Borges <douglasborges19@gmail.com>
2022-12-26 11:59:42 -0300

    add inheritance-03

---
+++ inheritance-03/Course/Entities/Account.cs
@@ -0,0 +1,33 @@
+﻿namespace Course.Entities
+{
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+        private const double WITHDRAWL_RATE = 5.0;
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        //"virtual" indica que pode ser sobrescrito em uma subclasse
+        public virtual void WithDraw(double amount)
+        {
+            Balance -= amount + WITHDRAWL_RATE;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ inheritance-03/Course/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Course.Entities;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1001, "Alex", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Bob", 500.0, 0.01);
+
+            acc1.WithDraw(10.0);
+            acc2.WithDraw(10.0);
+
+            //saque + 5.0 (taxa account)
+            Console.WriteLine(acc1.Balance);
+
+            // saque + 5.0 (taxa account) + 2.0 (taxa savings)
+            Console.WriteLine(acc2.Balance);
+        }
+    }
+}
