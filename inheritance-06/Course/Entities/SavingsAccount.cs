018a43fd0653e747561778e20e04da276f2723ef
Douglas Borges <douglasborges19@gmail.com>
2022-12-26 18:24:37 -0300

    abstract class

---
+++ inheritance-06/Course/Entities/SavingsAccount.cs
@@ -0,0 +1,32 @@
+﻿namespace Course.Entities
+{
+    //"sealed" evita que a classe possa ser herdada
+    internal sealed class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance = Balance * InterestRate;
+        }
+
+        //"override" indica que esta sobrescrevendo um método da superclasse
+        //"sealed" evita que o método seja sobrescrito novamente
+        public sealed override void WithDraw(double amount)
+        {
+            //"base" chama a implementação do método na superclasse
+            base.WithDraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
