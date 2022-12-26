93947baf9d631160d4a729b82d7fab078177a79c
Douglas Borges <douglasborges19@gmail.com>
2022-12-26 20:48:56 -0300

    abstract methods exercise

---
+++ inheritance-08/Course/Entities/Company.cs
@@ -0,0 +1,24 @@
+﻿namespace Course.Entities
+{
+    internal class Company : TaxPayer
+    {
+        public double NumberOfEmployees { get; set; }
+
+        public Company(string name, double anualIncome, int numberOfEmployees) : base(name, anualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double Tax()
+        {
+            if (NumberOfEmployees > 10)
+            {
+                return AnualIncome * 0.14;
+            }
+            else
+            {
+                return AnualIncome * 0.16;
+            }
+        }
+    }
+}
